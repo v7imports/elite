ac91bede9edb1deb95b78439f28cee4fb9a56c97
Victor <victorluispinheiro808@gmail.com>
2026-09-22 10:26:39 -0300

    10

--- firebase/js/firebase-config.js
+++ firebase/js/firebase-config.js
@@ -1,22 +1,3 @@
-console.log(">>> firebase-config.js FOI CARREGADO <<<");
+alert("FIREBASE-CONFIG FOI EXECUTADO!");
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDHXK2yV8ZliQCYU3LSGnmJmWe2gZ-8IMc",
-  authDomain: "v7imports.firebaseapp.com",
-  projectId: "v7imports",
-  storageBucket: "v7imports.firebasestorage.app",
-  messagingSenderId: "313801918256",
-  appId: "1:313801918256:web:7362aa27dbcad009ba2af5"
-};
-
-console.log("Firebase disponível:", typeof firebase);
-
-firebase.initializeApp(firebaseConfig);
-
-console.log("Firebase inicializado:", firebase);
-
-window.auth = firebase.auth();
-window.db = firebase.firestore();
-
-console.log("AUTH CRIADO:", window.auth);
-console.log("DB CRIADO:", window.db);
+console.log("FIREBASE-CONFIG FOI EXECUTADO!");
